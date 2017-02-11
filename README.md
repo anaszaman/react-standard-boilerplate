8ae41b8e4d87e8ae5a73aec0e875d713174c8c50
Anas Zaman <anaszaman.az@gmail.com>
2017-02-11 16:21:39 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# react-standard-boilerplate
+
+This is an opinionated React boilerplate using [create-react-app](https://www.npmjs.com/package/create-react-app), with [standardjs](http://standardjs.com/index.html) linting.
+
---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <div className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h2>React-Standard-Boilerplate</h2>
+        </div>
+        <p className='App-intro'>
+          Hello World!
+        </p>
+      </div>
+    )
+  }
+}
+
+export default App
